a0aac26a31603ace18b6275b65046ae389946621
Alain2020 <favouralain@yahoo.com>
2023-11-11 01:41:28 +0000

    Add simple-interest.sh script

---
+++ simple-interest.sh
@@ -0,0 +1,6 @@
+git add CONTRIBUTING.md
+git commit -m "Add contribution guidelines
+
+Provide clear instructions for bug reports, fixes, enhancements, and ideas. All contributions are welcome.
+
+Co-authored-by: Upkar Lidder <favouralain@yahoo.com>"
